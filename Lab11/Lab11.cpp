05a5873a8cce5cb616fed443018d503612b6d832
peter20011 <piotr.warcholek@student.pk.edu.pl>
2022-05-23 11:33:26 +0200

    started

--- Lab11/Lab11.cpp
+++ Lab11/Lab11.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
+#include <time.h>
 #define D 1
 #define TETA 0.1
 #define TMAX 2
-#define TMIN 0
 #define ITERACJE 100
 #define EPSILON 1e-16
 using namespace std;
@@ -65,10 +65,7 @@ double** anal_solution(double x_min, double x_max, double tmax, double h, double
 }
 //klasyczna metoda bezpośrednia
 double KMB(double x_min, double x_max, double tmax, double h, double dt){
-      int count_x = (int) ((x_max - x_min) / h),
-            count_t = (int) (tmax / dt),
-            ex_time = clock(),
-            i, k;
+      int count_x = (int) ((x_max - x_min) / h),count_t = (int) (tmax / dt),ex_time = clock(),i, k;
     double **u = new double*[count_x],
             lambda = D * dt / (h * h);
     for (i = 0; i < count_x; i++) {
@@ -103,10 +100,7 @@ double KMB(double x_min, double x_max, double tmax, double h, double dt){
 }
 
 double Laasonen_Tom(double x_min, double x_max, double tmax, double h, double dt){
-        int count_x = (int) ((x_max - x_min) / h),
-            tcount = (int) (tmax / dt),
-            ex_time = clock(),
-            i;
+        int count_x = (int) ((x_max - x_min) / h),count_t = (int) (tmax / dt),ex_time = clock(),i;
     double **result = new double*[tcount],
             *l = new double[count_x - 1],
             *d = new double[count_x],
@@ -155,4 +149,119 @@ double Laasonen_Tom(double x_min, double x_max, double tmax, double h, double dt
     return ex_time;
 }
 
-double Laasonen_SOR(double x_min, double x_max, double tmax, double h, double dt)
+void SOR(double **A, double *x_0, const double *b, double *x_n) {
+    double omega = 1./2.;
+    double *e = new double[size];
+    double *residual = new double[size];
+
+    for(int n = 0; n < ITERACJE; n++) { //tworzymy pętle główną, której ograniczeniem jest liczba iteracji	
+
+        double *vectorDUxn = new double[size];
+
+        for(int i = 0; i < size; i++) {
+            double sum = 0.0;
+            for(int j = 0; j < size; j++) {
+                double U_Element = 0;
+                if(i < j) {
+                    U_Element = A[i][j];
+                } else if (i == j) {
+                    U_Element = (1 - 1./omega) * A[i][j];
+                }
+                sum += U_Element * x_0[j];
+            }
+            vectorDUxn[i] = sum;
+        }
+
+        double *y = new double[size];
+
+        for(int l = 0; l < size; l++) {
+            y[l] = -vectorDUxn[l] + b[l];
+        }
+
+        x_n = calculateVectorL(A, y, omega);
+
+        for(int l = 0; l < size; l++) {
+            e[l] = x_n[l] - x_0[l];
+            residual[l] = fabs(A[l][0]*x_n[0] + A[l][1]*x_n[1] + A[l][2]*x_n[2] + A[l][3]*x_n[3] - b[l]); // przemnazamy wiersze razy wektor xx i odejmujemy wektor b
+            x_0[l] = x_n[l];
+        }
+
+        cout << "x_n: ";
+        for(int i = 0; i < size; i++) {
+            cout << x_n[i] << " ";
+        }
+        cout << endl;
+
+        cout << "e = " << norm_max(e) << "\t\t|f(x)| = " << norm_max(residual);
+        cout << endl;
+
+        if(norm_max(e) <= TOLX && norm_max(residual) <= TOLF) {
+             cout<<"Zakończono na podstawie kreyterium numer 2 i 3"<<endl;
+            return;
+        }
+
+        cout << endl;
+        cout << endl;
+    }
+}
+
+double *calculateVectorL(double **L, const double* vectorB, double omega){
+    double *temp=new double[size];
+    temp[0] = vectorB[0]/(L[0][0]*(1./omega));
+    for(int i=1;i<size;i++){
+        double sum=0;
+        for(int j=0;j<i;j++){
+            sum+=L[i][j]*temp[j];
+        }
+
+       temp[i] = (vectorB[i] - sum)/(L[i][i]*((1./omega)));
+    }
+
+    return temp;
+}
+
+double norm_max(double *vector){
+    double biggest=fabs(vector[0]);
+    for(int i=0;i<3;i++){
+        if(fabs(vector[i]>biggest)){
+            biggest=fabs(vector[i]);
+        }
+    }
+
+    return biggest;
+}
+
+double Laasonen_SOR(double x_min, double x_max, double tmax, double h, double dt){
+    int count_x = (int) ((x_max - x_min) / h),tcount = (int) (tmax / dt),ex_time = clock(),i;
+}
+
+
+int main(){
+    double x_max=6*pow(D*(TETA+TMAX),(double)1/2);
+    double h=1.0, dt=0.4*h*h/D; //lambda 0.4-> KMB jest wtedy stabilna numerycznie 
+    cout << "Rozwiazywanie rownania rozniczkowego czastkowego (typu Dyfuzji)" << endl;
+    cout<<"KMB"<<endl;
+    cout<<"h\terr"<<endl;
+    while (h>0.009)
+    {
+        cout << h<<"\t";
+        KMB(-x_min,x_max,TMAX,h,dt);
+        cout<<endl;
+        h=h*0.7;
+        dt=0.4*h*h/D;
+    }
+
+    h=1.0;
+    dt=h*h/D; // lambda 1 dla metody pośredniej Lassonen
+    cout<<"Lassonen+Thomas"<<endl;
+    while(h>0.009){
+        cout<<h<<"\t";
+        Laasonen_Tom(-xmax, xmax, TMAX, h, dt);
+        cout<<endl;
+        h=h*0.7;
+        dt = h * h / D;
+    }
+
+    return 0;
+
+}
